031c12644bf157102aa0dd7987b7bb575e632e44
nthouliss <nthouliss@gmail.com>
2023-09-20 15:29:16 +1000

    Add call to function in verify script

--- scripts/verify/index.js
+++ scripts/verify/index.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import { getExtensionDetails } from "../common/extensionDetails.js";
 
 function createDiscordVerificationPost(id, data) {
   const text = `## Extension Verfication Request\n\n*${data.title}* would like to be verified.\nIt can be installed via this [store page](https://extensions.owlbear.rodeo/${id}).\n\nTo be verified the extension must meet these criteria:\n\n- Extension design uses accessible colors\n- Extension design uses accessible font sizes\n- Extension is legible with Owlbear Rodeo’s light and dark theme\n- Extension is fully functional on mobile devices (1)\n- Extension is fully functional across all major browsers (2)\n- Extension requires no other extensions to be installed\n- Extension functions in a private browsing window or with cookies disabled\n- Extension makes proper use of the Owlbear Rodeo APIs (3)\n- Extension functions in all configurations of an Owlbear Rodeo Room (4)\n- Extension provides user support for queries, issues and requests\n- Extension has no known bugs\n- Extension manifest is hosted on a custom domain controlled by the extension developer\n\nNotes:\n\n1) This includes iPhone’s, Android devices as well as tablets such as an iPad.\n\n2) Major browsers include Chrome, Firefox and Safari\n\n3) For example the Scene API is only used to store data that shares the Scene lifecycle\n\n4) Valid configurations include a Room with a Scene open and no Scene open\n\nAlso note that the term “fully functional” above means all extension functionally works correctly whereas the term “functional” means the extension still functions but some features may not be available.`;
@@ -29,3 +30,5 @@ export async function sendDiscordWebhook() {
     console.log(JSON.stringify(await response.text(), null, 2));
   }
 }
+
+await sendDiscordWebhook();
